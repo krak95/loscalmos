569c01822ecda8b242b1519debe496fe6c3f9876
José Amorim <jpoa95@gmail.com>
2022-10-27 16:55:04 +0100

    tempjs slideshow

--- tempjs/js.js
+++ tempjs/js.js
@@ -1,16 +1,18 @@
-function draggable(){
-$('#deck1').draggable({ revert: true });
-$('#deck1').draggable( "option", "revert", true);
-$('#drophere').droppable({
-hoverClass:"hover",
-activeClass:"active",
-drop: function(){
-$(this).text('olá');
-$('#deck1').text('olá');
-setTimeout(() => {
-$('#drophere').text('');
-    
-}, 222);
+/*$(document).ready(function(){
+let leng = $('.container div').length
+let nrshow = 2;
+let nrofpages = leng/nrshow;
+for(i=0;i < nrofpages;i++){
+let pagenumber = i+1;
+$('.mainbox').append('<a page='+i+'>'+pagenumber+'</a>')
 }
-})
-}
+$('.container div').slice(nrshow,leng).hide();
+$('a').click(function(){
+let currentpage = $(this).attr('page');
+let firstitemshown = currentpage * nrshow;
+let lastitemshown = firstitemshown + nrshow;
+$('.container div').slice(0,nrshow).show();
+$('.container div').hide().slice(firstitemshown,lastitemshown).show();
+})*/
+
+
