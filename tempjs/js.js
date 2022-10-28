bf9db58b8df96f17f1e092c42f6a390103456edc
José Amorim <jpoa95@gmail.com>
2022-10-28 08:02:48 +0100

    naruto shuffle

--- tempjs/js.js
+++ tempjs/js.js
@@ -16,3 +16,59 @@ $('.container div').hide().slice(firstitemshown,lastitemshown).show();
 })*/
 
 
+
+<script>
+    
+    setInterval(() => {
+        
+  
+let show = $('.shown').show().css('display','flex');
+let next = $('.shown').next().css('display','flex');
+let prev = $('.shown').prev().css('display','flex');
+$('.shown').next().addClass('next');
+$('.shown').prev().addClass('prev');
+$('.shown').prevAll().not('.prev').hide();
+$('.shown').nextAll().not('.next').hide();
+
+
+$(next).on('click',function(){
+    let el = this;
+   setTimeout(() => {
+  
+    $(el).removeClass('next').addClass('shown');
+    $('.shown').css('display','flex');
+    $('.shown').next().addClass('next').show();
+    $('.shown').prev().addClass('prev').show();
+}, 0);
+
+    $('.container div').removeClass('shown');
+    $('.container div').removeClass('next');
+    $('.container div').removeClass('prev');
+    
+})
+$(prev).on('click',function(){
+    let el = this;
+   setTimeout(() => {
+    $(el).removeClass('prev').addClass('shown');
+    $('.shown').css('display','flex');
+    $('.shown').next().addClass('next').show();
+    $('.shown').prev().addClass('prev').show();
+
+   }, 0);
+    
+    $('.container div').removeClass('shown');
+    $('.container div').removeClass('next');
+    $('.container div').removeClass('prev');
+    
+})
+
+let backG = $('.shown img').attr("alt",)
+let bodyG = document.getElementById('mainbox');
+let backG1 = bodyG.style.background = "url("+backG+")";
+
+}, 0);
+
+
+
+/*
+</script>
