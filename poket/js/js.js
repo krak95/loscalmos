ed9417c3efd9e1f65829c0fb541862a346662659
José Amorim <jpoa95@gmail.com>
2022-10-29 22:21:32 +0100

    poket->user will now logoff on page close/refresh

--- poket/js/js.js
+++ poket/js/js.js
@@ -7,7 +7,7 @@ $('#friendsoff').hide();
 $('.success').css('display','none');
 $('.error').css('display','none');
 
-    $('#return').on('click',function(){
+$('#return').on('click',function(){
 $('.mainmenu').show();
 $('.play-maindiv').hide();
 $('.form-signup').hide();
@@ -23,7 +23,7 @@ $('.mainmenu').hide();
 $('.form-signup').show();
 })
 
-    $('#signup-submit').on('click',function(){
+$('#signup-submit').on('click',function(){
 var username = $('#uname').val();
 var user_pwd = $('#email').val();
 var user_email = $('#pwd').val();
@@ -38,12 +38,12 @@ alert('new user');
 })
 
 $('#loguname,#logpwd').on('keypress', function(event){
-    if (event.key === "Enter") {
-    event.preventDefault();
-    $('#login-submit').click();
-    }
+if (event.key === "Enter") {
+event.preventDefault();
+$('#login-submit').click();
+}
 })
-    $('#login-submit').on('click',function(){
+$('#login-submit').on('click',function(){
 var username = $('#loguname').val();
 var user_pwd = $('#logpwd').val();
 $.ajax({
@@ -51,36 +51,59 @@ type:'post',
 url:'php/users/login.php',
 data:{username:username,user_pwd:user_pwd},
 success: function(data){console.log(data)
-    if(data == 'connect'){
-        $('#login-submit').addClass('success');
-        $('#login-submit').text('Success');
-        //$('.success').css('display','flex');
-        setTimeout(() => {
-        $(location).prop('href','');
-        }, 1500);
-    }
-    if(data == 'connection error'){
-        $('#login-submit').addClass('error');
-        $('#login-submit').text('Error');
-        setTimeout(() => {
-            $('#login-submit').removeClass('error');
-            $('#login-submit').text('Login');
-            }, 1500);
-    }
-}
-})
-})
-$('#logout').on('click',function(){
-$.ajax({
-url:'php/users/logout.php',
-success: function(){
-alert('destroy');
+if(data == 'connect'){
+$('#login-submit').addClass('success');
+$('#login-submit').text('Success');
+//$('.success').css('display','flex');
+setTimeout(() => {
 $(location).prop('href','');
+}, 1500);
+}
+if(data == 'connection error'){
+$('#login-submit').addClass('error');
+$('#login-submit').text('Error');
+setTimeout(() => {
+$('#login-submit').removeClass('error');
+$('#login-submit').text('Login');
+}, 1500);
+}
 }
 })
 })
 
-    $('#profile').on('click',function(){
+
+function timedlogout(){
+$('body').mouseleave(function(e){
+if($('.timedlogout').length == 0){ 
+console.log('bye')
+setTimeout(() => {
+$('.screen').prepend('<div class="timedlogout"><h3>Do you want to keep logged in?</h3><button id="logoutyes">yes</button><button id="logoutno">no</button></div>')
+$('#logoutyes').click(function(){
+console.log('close')
+clearTimeout(countdown);
+$('.timedlogout').remove();
+})
+const countdown = setTimeout(() => {
+$('#logout').click();
+}, 60000*5);
+}, 60000*60);
+}
+})
+function logout(){
+$('#logout').on('click',function(){
+$.ajax({
+url:'php/users/logout.php',
+success: function(){
+$(location).prop('href','');
+}
+})
+})
+}
+logout();
+}
+timedlogout()
+
+$('#profile').on('click',function(){
 $('.mainmenu').hide();
 $.ajax({
 url:'php/users/profile.php',
@@ -93,7 +116,7 @@ $('.profile-div table').html(data);
 })
 })
 
-    $('#friendson').on('click',function(){
+$('#friendson').on('click',function(){
 $('.friends-btn-on').addClass('friendsanime');
 $('.friends-btn-off').addClass('friendsanime');
 $('.backcurtain').show();
@@ -112,7 +135,7 @@ url:'php/users/userstatus.php',
 success:function(data){
 setTimeout(() => {
 $('.friends ol').html(data);
-    
+
 }, 5);
 }
 })
@@ -120,15 +143,15 @@ var refresh = setInterval(() => {
 $.ajax({
 url:'php/users/userstatus.php',
 success:function(data){
-    setTimeout(() => {
-    $('.friends ol').html(data);
-        
-    }, 5);
+setTimeout(() => {
+$('.friends ol').html(data);
+
+}, 5);
 }
 })
 }, 3000);
 
-    $('#friendsoff').on('click',function(){
+$('#friendsoff').on('click',function(){
 $('.friends-btn-on').removeClass('friendsanime');
 $('.friends-btn-off').removeClass('friendsanime');
 $('.backcurtain').hide();
@@ -142,8 +165,13 @@ $('#friendsoff').hide();
 })
 })
 
-
-
+function endsession(){
+$(window).on('beforeunload', function(event){
+$('.screen').append('<div class="closesession">Closing session...</>')
+$('#logout').click();
+});
+}
+endsession();
 
 
 //END OF DOCUMENT//
