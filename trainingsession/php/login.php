8df921ac576ed3e3e950d043128cf45e5bc8a7c2
José Amorim <jpoa95@gmail.com>
2022-11-03 02:35:19 +0000

    ftp api (first steps)

--- trainingsession/php/login.php
+++ trainingsession/php/login.php
@@ -1,9 +1,32 @@
 <?php
-session_start();
-if(isset($_POST['name'])){
-    $name = $_POST['name'];
-    $_SESSION['name'] = $name;
+$path    = 'files';
+if ($handle = opendir($path)) {
+    while (false !== ($file = readdir($handle))) {
+        if ('.' === $file) continue;
+        if ('..' === $file) continue;
+        ?>
+        <ol>
+            <li class='download' data-id='<?=$path?>\<?=$file?>'> <?=$file?> </li>
+        </ol>
+    <?php }
+    closedir($handle);
 }
-
-header('location:session.php');
-    ?>
+?>
+<script src="jq.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.download').on('click',function(){
+            let el = this;
+            let path = $(el).data('id');
+            $.ajax({
+                url:'download.php/'+path,
+                type:'get',
+                data:{path:path},
+                success:function(data){
+                    window.location.replace(path);
+                }
+            })
+        })
+    }
+    )
+</script>
